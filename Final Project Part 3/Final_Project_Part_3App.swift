e1d387a9fac35a4808caac66da04952c4e87ba42
Tim Stack <tstack43215@yahoo.com>
2023-04-28 16:05:40 -0500

    Initial Commit

---
+++ Final Project Part 3/Final_Project_Part_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Final_Project_Part_3App.swift
+//  Final Project Part 3
+//
+//  Created by IIT PHYS 440 on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_Project_Part_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Final Project Part 3UITests/Final_Project_Part_3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Final_Project_Part_3UITestsLaunchTests.swift
+//  Final Project Part 3UITests
+//
+//  Created by IIT PHYS 440 on 4/28/23.
+//
+
+import XCTest
+
+final class Final_Project_Part_3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
